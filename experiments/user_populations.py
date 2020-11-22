bb0e81fd604c1042d5439303c1170228c9885438
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-11-22 17:27:09 -0300

    hechos algunos endpoints correspondientes a los usuarios, hay que revisar los queries de cypher y el formato en el que devuelven los resultados

--- experiments/user_populations.py
+++ experiments/user_populations.py
@@ -4,7 +4,7 @@ from Neo4jConnection import Neo4jConnection
 #conectando con mongo
 mongo = MongoClient('localhost:27017')
 mongodb = mongo['bd2tp']
-users_coll = mongodb['users']
+users_coll = mongodb['usuarios']
 
 #conectando con neo4j
 neo4j = Neo4jConnection(uri='bolt://localhost:7687')
@@ -13,16 +13,19 @@ neo4j = Neo4jConnection(uri='bolt://localhost:7687')
 users = [
     {
         'nombre': 'gaston',
+        'legajo': 12345,
         'descripcion': None,
         'foto': None
     },
     {
         'nombre': 'micaela',
+        'legajo': 56789,
         'descripcion': None,
         'foto': None
     },
     {
         'nombre': 'lautaro',
+        'legajo': 87643,
         'descripcion': None,
         'foto': None
     }
@@ -33,13 +36,14 @@ users_coll.insert_many(users)
 
 #agregar nodos a neo
 for u in users:
-    q = "CREATE (u:Usuario {{nombre: '{}'}});".format(u['nombre'])
+    q = "CREATE (u:Usuario {{nombre:'{}', legajo:'{}'}});".format(u['nombre'], u['legajo'])
 
 #agregar las relaciones a neo
-u1 = users[0]['nombre']
-u2 = users[1]['nombre']
-q = "MATCH (u1:User {{nombre: '{}'}}), \
-           (u2:User {{nombre: '{}'}}}) \
+u1 = users[0]['legajo']
+u2 = users[1]['legajo']
+q = "MATCH (u1:User {{legajo: '{}'}}), \
+           (u2:User {{legajo: '{}'}}}) \
     CREATE (m1)-[r:amigoDe]->(m2),\
     CREATE (m2)-[r:amigoDe]->(m1);\
     ".format(u1, u2)
+neo4j.query(q)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Cookie
 from pymongo import MongoClient
 from Neo4jConnection import Neo4jConnection
 import json
@@ -10,6 +10,7 @@ app = FastAPI()
 mongo = MongoClient('mongo:27017')
 mongodb = mongo['bd2tp']
 materias_coll = mongodb['materias']
+users_coll = mongodb['usuarios']
 
 #conectando con neo4j
 neo4j = Neo4jConnection(uri='bolt://neo4j:7687')
@@ -18,6 +19,9 @@ neo4j = Neo4jConnection(uri='bolt://neo4j:7687')
 @app.get("/")
 def read_root():
     return {"Hello": "world"}
+
+
+############################## MATERIAS #################################
 
 @app.get('/all_courses')
 def get_all_courses():
@@ -30,6 +34,42 @@ def get_course(codigo_materia: str):
     cursor = materias_coll.find({'codigo': codigo_materia}, {'_id': False})
     return {'materia' : cursor[0]}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/current_user_courses')
+def read_item(user_id: Optional[str] = None):
+    if not user_id:
+        return {'Error': 'user_id cookie not set'}
+    q = "MATCH (m:Materia), (u:User {{ legajo: '{}'}})\
+         RETURN (u)-[:cursando]->(m)".format(user_id)
+    result = neo4j.query(q)
+    return {"mis_materias": result}
+
+
+############################## REVIEWS #################################
+
+
+############################## USUARIOS #################################
+
+@app.get('/users/{legajo}')
+def get_user(legajo: int):
+    cursor = users_coll.find({'legajo': legajo}, {'_id': False})
+    return {'usuario' : cursor[0]}
+
+@app.get('/friends')
+def get_friends(user_id: Optional[str] = Cookie(None)):
+    if not user_id:
+        return {'Error': 'No está logueado'}
+    q = "MATCH (u1:User {{legajo: '{}'}}), (u2:User) \
+        RETURN (u2)<-[r:amigoDe]-(u1)"
+    result = neo4j.query(q)
+    return {'amigos': result}
+
+@app.post('/friend_request/{legajo}')
+def add_friend(legajo: int, user_id: Optional[str] = Cookie(None)):
+    q = "MATCH (u1:User {{legajo: '{}'}}), \
+           (u2:User {{legajo: '{}'}}}) \
+    CREATE (m1)-[r:amigoDe]->(m2),\
+    CREATE (m2)-[r:amigoDe]->(m1);\
+    ".format(user_id, legajo)
+    result = neo4j.query(q)
+    return {'friend_request_result': result}
+
